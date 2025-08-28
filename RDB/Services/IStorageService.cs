9659496985f3d5966f9f2ae5451d514f407dfa56
HyperRush Network <hyperrushnetwork@gmail.com>
2025-08-28 22:52:56 +0200

    Update IStorageService.cs

--- RDB/Services/IStorageService.cs
+++ RDB/Services/IStorageService.cs
@@ -1,15 +1,10 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using RDB.Models;
-using System.Text.Json;
 
-namespace RDB.Services
+namespace RDB.Services;
+
+public interface IStorageService
 {
-    public interface IStorageService
-    {
-        Task<IndexEntry> StoreItemAsync(string type, JsonElement payload);
-        Task<ItemEnvelope?> GetItemAsync(string type, string id);
-        Task<IEnumerable<IndexEntry>> ListTypeAsync(string type, int skip = 0, int take = 100);
-        Task<IEnumerable<IndexEntry>> StoreBatchAsync(string type, IEnumerable<JsonElement> payloads);
-    }
+    Task SaveItemAsync(ItemEnvelope item);
+    Task<ItemEnvelope?> GetItemAsync(string type, string id);
+    Task<List<ItemEnvelope>> GetAllItemsAsync(string type);
 }
