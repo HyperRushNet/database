4c7704b6cd6db20c69be6cb86e1dc9cee8a5d77f
HyperRush Network <hyperrushnetwork@gmail.com>
2025-08-28 22:36:21 +0200

    Update Program.cs

--- RDB/Program.cs
+++ RDB/Program.cs
@@ -5,13 +5,32 @@ using RDB.Services;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
+
 var dataDir = Environment.GetEnvironmentVariable("DATA_DIR") ?? "data";
+
 builder.Services.AddSingleton<DatabaseService>(sp => new DatabaseService(dataDir, batchFlushThreshold: 8, batchFlushInterval: TimeSpan.FromSeconds(3)));
 builder.Services.AddSingleton<IStorageService>(sp => sp.GetRequiredService<DatabaseService>());
 builder.Services.AddSingleton<BatchFlushService>();
 builder.Services.AddHostedService(sp => sp.GetRequiredService<BatchFlushService>());
+
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
+
 app.UseRouting();
+
+app.UseCors();
+
 app.UseEndpoints(endpoints => endpoints.MapControllers());
+
 app.Run();
