80ff07ee3ba2d30caa66466fdad1a15d2793a320
HyperRush Network <hyperrushnetwork@gmail.com>
2025-08-28 22:56:29 +0200

    Delete RDB directory

--- RDB/Models/IndexEntry.cs
+++
@@ -1,7 +0,0 @@
-namespace RDB.Models;
-
-public class IndexEntry
-{
-    public string Id { get; set; } = null!;
-    public string Type { get; set; } = null!;
-}
--- RDB/Models/ItemEnvelope.cs
+++
@@ -1,9 +0,0 @@
-namespace RDB.Models;
-
-public class ItemEnvelope
-{
-    public string Id { get; set; } = null!;
-    public string Type { get; set; } = null!;
-    public DateTime CreatedAt { get; set; }
-    public object? Payload { get; set; }
-}
