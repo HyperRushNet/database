f0c3d9b2752138780cae0e6bb84b0a1eb1af66f0
HyperRush Network <hyperrushnetwork@gmail.com>
2025-08-28 22:53:43 +0200

    Update Program.cs

--- RDB/Program.cs
+++ RDB/Program.cs
@@ -1,36 +1,20 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using RDB.Services;
-using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var dataDir = Environment.GetEnvironmentVariable("DATA_DIR") ?? "data";
-
-builder.Services.AddSingleton<DatabaseService>(sp => new DatabaseService(dataDir, batchFlushThreshold: 8, batchFlushInterval: TimeSpan.FromSeconds(3)));
-builder.Services.AddSingleton<IStorageService>(sp => sp.GetRequiredService<DatabaseService>());
-builder.Services.AddSingleton<BatchFlushService>();
-builder.Services.AddHostedService(sp => sp.GetRequiredService<BatchFlushService>());
-
+builder.Services.AddSingleton<IStorageService, DatabaseService>();
 builder.Services.AddControllers();
-
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.AllowAnyOrigin()
-              .AllowAnyMethod()
-              .AllowAnyHeader();
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
     });
 });
 
 var app = builder.Build();
 
-app.UseRouting();
-
 app.UseCors();
-
-app.UseEndpoints(endpoints => endpoints.MapControllers());
+app.MapControllers();
 
 app.Run();
