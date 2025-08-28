06ecf4240b186428d212b4d03f00644831255cfa
HyperRush Network <hyperrushnetwork@gmail.com>
2025-08-28 22:33:22 +0200

    Create Program.cs

---
+++ RDB/Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RDB.Services;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+var dataDir = Environment.GetEnvironmentVariable("DATA_DIR") ?? "data";
+builder.Services.AddSingleton<DatabaseService>(sp => new DatabaseService(dataDir, batchFlushThreshold: 8, batchFlushInterval: TimeSpan.FromSeconds(3)));
+builder.Services.AddSingleton<IStorageService>(sp => sp.GetRequiredService<DatabaseService>());
+builder.Services.AddSingleton<BatchFlushService>();
+builder.Services.AddHostedService(sp => sp.GetRequiredService<BatchFlushService>());
+builder.Services.AddControllers();
+var app = builder.Build();
+app.UseRouting();
+app.UseEndpoints(endpoints => endpoints.MapControllers());
+app.Run();
