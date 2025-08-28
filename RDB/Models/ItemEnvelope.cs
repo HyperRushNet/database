d722e3e63e3d180cdaff337b9afac0a6f7d9a462
HyperRush Network <hyperrushnetwork@gmail.com>
2025-08-28 22:52:33 +0200

    Update ItemEnvelope.cs

--- RDB/Models/ItemEnvelope.cs
+++ RDB/Models/ItemEnvelope.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Text.Json;
+namespace RDB.Models;
 
-namespace RDB.Models
+public class ItemEnvelope
 {
-    public class ItemEnvelope
-    {
-        public string Id { get; set; } = Guid.NewGuid().ToString("n");
-        public string Type { get; set; } = "";
-        public string Path { get; set; } = "";
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public JsonElement Payload { get; set; }
-        public string ToJson() => JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = false });
-    }
+    public string Id { get; set; } = null!;
+    public string Type { get; set; } = null!;
+    public DateTime CreatedAt { get; set; }
+    public object? Payload { get; set; }
 }
