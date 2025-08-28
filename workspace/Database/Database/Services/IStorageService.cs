273a926a18a5d4ddf27a29d8a2a569af40b6dbd9
HyperRush Network <hyperrushnetwork@gmail.com>
2025-08-28 22:19:34 +0200

    Create IStorageService.cs

---
+++ workspace/Database/Database/Services/IStorageService.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Database.Models;
+using System.Text.Json;
+
+namespace Database.Services
+{
+    public interface IStorageService
+    {
+        Task<IndexEntry> StoreItemAsync(string type, JsonElement payload);
+        Task<ItemEnvelope?> GetItemAsync(string type, string id);
+        Task<IEnumerable<IndexEntry>> ListTypeAsync(string type, int skip = 0, int take = 100);
+        Task<IEnumerable<IndexEntry>> StoreBatchAsync(string type, IEnumerable<JsonElement> payloads);
+    }
+}
