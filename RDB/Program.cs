b2ac1aa93e79cb95c23ca8a7831b9ef2bc4029af
HyperRush Network <hyperrushnetwork@gmail.com>
2025-08-29 01:14:42 +0200

    Overwrite RDB/Program.cs

--- RDB/Program.cs
+++ RDB/Program.cs
@@ -1,19 +1,20 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
 using RDB.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSingleton<DatabaseService>();
-builder.Services.AddCors(p => p.AddDefaultPolicy(b => b.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+builder.Services.AddSingleton<IStorageService, DatabaseService>();
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
 app.UseCors();
-
 app.MapControllers();
-app.MapFallback(context => { context.Response.StatusCode = 204; return Task.CompletedTask; });
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-app.Urls.Add($"http://*:{port}");
 app.Run();
