b1d7ee6f3a4c6369f2d531bedd209320a2a54576
HyperRush Network <hyperrushnetwork@gmail.com>
2025-08-28 23:01:33 +0200

    Create IStorageService.cs

---
+++ RDB/Services/IStorageService.cs
@@ -0,0 +1,11 @@
+using RDB.Models;
+
+namespace RDB.Services;
+
+public interface IStorageService
+{
+    Task SaveItemAsync(ItemEnvelope item);
+    Task<ItemEnvelope?> GetItemAsync(string type, string id);
+    Task<List<ItemEnvelope>> GetAllItemsAsync(string type);
+    Task<bool> DeleteItemAsync(string type, string id);
+}
