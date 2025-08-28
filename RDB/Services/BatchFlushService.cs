ca1b09a0ba3134bf99dd57dfc97c6fee90ace0e5
HyperRush Network <hyperrushnetwork@gmail.com>
2025-08-28 22:35:15 +0200

    Create BatchFlushService.cs

---
+++ RDB/Services/BatchFlushService.cs
@@ -0,0 +1,12 @@
+using Microsoft.Extensions.Hosting;
+
+namespace RDB.Services
+{
+    public class BatchFlushService : IHostedService
+    {
+        private readonly DatabaseService _database;
+        public BatchFlushService(DatabaseService database){_database = database;}
+        public Task StartAsync(System.Threading.CancellationToken cancellationToken){return Task.CompletedTask;}
+        public Task StopAsync(System.Threading.CancellationToken cancellationToken){_database.Dispose(); return Task.CompletedTask;}
+    }
+}
