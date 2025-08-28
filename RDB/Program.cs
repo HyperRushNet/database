296f474a1a92b203492828871a02336863537c0a
HyperRush Network <hyperrushnetwork@gmail.com>
2025-08-28 23:00:50 +0200

    Create Program.cs

---
+++ RDB/Program.cs
@@ -0,0 +1,20 @@
+using RDB.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IStorageService, DatabaseService>();
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+app.UseCors();
+app.MapControllers();
+
+app.Run();
