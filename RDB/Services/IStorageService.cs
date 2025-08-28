fd10925ce9f198a558efa98f9b8ccf430720d3c8
HyperRush Network <hyperrushnetwork@gmail.com>
2025-08-29 01:14:48 +0200

    Overwrite RDB/Services/IStorageService.cs

--- RDB/Services/IStorageService.cs
+++ RDB/Services/IStorageService.cs
@@ -4,8 +4,8 @@ namespace RDB.Services;
 
 public interface IStorageService
 {
-    ItemEnvelope AddItem(string type, object payload);
-    ItemEnvelope? GetItem(string type, string id);
-    IEnumerable<ItemEnvelope> GetItems(string type);
-    bool DeleteItem(string type, string id);
+    Task SaveItemAsync(ItemEnvelope item);
+    Task<ItemEnvelope?> GetItemAsync(string type, string id);
+    Task<List<ItemEnvelope>> GetAllItemsAsync(string type);
+    Task<bool> DeleteItemAsync(string type, string id);
 }
