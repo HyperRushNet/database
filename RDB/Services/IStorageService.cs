2f66606a7f8ac4dbdfb54d4aef475fd84ef28071
HyperRush Network <hyperrushnetwork@gmail.com>
2025-08-29 18:09:46 +0200

    Update IStorageService.cs

--- RDB/Services/IStorageService.cs
+++ RDB/Services/IStorageService.cs
@@ -6,6 +6,6 @@ public interface IStorageService
 {
     Task SaveItemAsync(ItemEnvelope item);
     Task<ItemEnvelope?> GetItemAsync(string type, string id);
-    Task<List<ItemEnvelope>> GetAllItemsAsync(string type);
+    Task<List<ItemEnvelope>> GetAllItemsAsync(string type, int skip = 0, int take = 100);
     Task<bool> DeleteItemAsync(string type, string id);
 }
